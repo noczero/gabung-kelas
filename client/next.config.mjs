b2432512a1a6d8df578329402ab63db82d2a333c
noczero <zeroonetm@gmail.com>
2022-07-03 16:30:42 +0700

    add contact as envar

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
         serverURL: process.env.BASE_SERVER_URL || 'http://localhost:8080',
         apiURL: process.env.BASE_API_URL_PREFIX || 'http://localhost:8080/api' , // will use in axios request
         clientURL: process.env.BASE_CLIENT_URL || 'http://localhost:3001'
+    },
+    contact : {
+        twitter : 'https://twitter.com/@gabungkelas',
+        instagram: 'https://instagram.com/gabungkelas',
+        phoneNumber : '+62 822 xxx xxx xxx',
+        address : 'Jl. Alamat, Kabupatan, Kota, Provinsi, Negara',
+        email : 'cs@gabungkelas.id'
     }
 }
 
