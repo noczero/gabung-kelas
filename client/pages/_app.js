1d4e19eb84bc8873079db5fe60f99dca01c22751
noczero <zeroonetm@gmail.com>
2022-07-03 16:28:13 +0700

    add toast notification, class on body, and footer section

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,12 +2,24 @@ import TopNav from "../components/TopNav";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import '../public/css/style.css';
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "../components/footer/Footer"
+import {useEffect} from "react";
 
 function MyApp({Component, pageProps}) {
+    useEffect(() =>
+    {
+        document.body.classList.add("d-flex");
+        document.body.classList.add("flex-column");
+        document.body.classList.add("min-vh-100");
+    });
     return (
     <>
+        <ToastContainer position="top-center"/>
         <TopNav/>
         <Component {...pageProps}/>
+        <Footer/>
     </>
     )
 }
